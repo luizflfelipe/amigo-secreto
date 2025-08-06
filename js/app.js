b754102c9f291c7317bead78efbe0267fb6340e2
Luiz Felipe <luisffelipe20@gmail.com>
2025-08-05 21:55:30 -0300

    Comentários aprimorados

--- js/app.js
+++ js/app.js
@@ -6,18 +6,18 @@ function adicionar(){
         alert('Informe o nome do amigo!');
         return;
     }
-    let listaAmigos = document.getElementById('lista-amigos'); //Nome na lista de amigos incluídos :)
+    let listaAmigos = document.getElementById('lista-amigos'); //Nomes da lista de amigos incluídos :)
     Amigos.push(amigoSecreto);
 
 
-    if (listaAmigos.textContent == ''){
-        listaAmigos.textContent = amigoSecreto;
+    if (listaAmigos.textContent == ''){ // Se lista de amigos estiver vazia
+        listaAmigos.textContent = amigoSecreto;     //Coloque o nome da pessoa digitado
     }else {
-        listaAmigos.textContent = listaAmigos.textContent + ', ' + amigoSecreto;  //Modifica os amigos incluídos colocando os novos nomes :)
+        listaAmigos.textContent = listaAmigos.textContent + ', ' + amigoSecreto;  //Mantendo os nomes ja colocados e colocando os novos nomes :)
     }
 
-    if (Amigos.includes(amigoSecreto)){
-        alert('Amigo já incluído na Lista');
+    if (Amigos.includes(amigoSecreto)){ //Se o array Amigos tiver o nome do amigoSecreto
+        alert('Amigo já incluído na Lista'); // Mensagem que já possui o mesmo nome
         return;
     }
     
@@ -26,16 +26,16 @@ function adicionar(){
 
 
 function sortear(){
-    if (Amigos.length < 2) {
-        alert('Adicione pelo menos 2 amigos!');
+    if (Amigos.length < 2) { //Verifica quantos nomes tem no Array(Lista), se tiver menos que 2
+        alert('Adicione pelo menos 2 amigos!'); //O codigo pede para adicionar pelo menos 2 amigos 
         return;
     }
-    embaralha(Amigos);     //Recebeu a função de embaralhar a Amigos (um Array)
+    embaralha(Amigos);     //Recebeu a função de embaralhar Array(Amigos)
     let sorteio = document.getElementById('lista-sorteio');         //Pega o campo no HTML que mostra a lista de Nomes sorteados;
 
-    for (let index = 0; index < Amigos.length; index++) {       // Documentação pronta, porém o for é quase igual em todas as ocasiões, let recebe index que pode ser usado como i; logo após ele pega o i e percorre o Array inteiro, e depois ele incrementa mais um no index original index = 0;
-        if (index == Amigos.length - 1){
-            sorteio.innerHTML = sorteio.innerHTML + Amigos[index] + '--->' + Amigos[0] + '<br>'
+    for (let index = 0; index < Amigos.length; index++) {       // Documentação pronta, porém o for é quase igual em todas as ocasiões, let recebe index que pode ser usado como i; logo após ele pega a variavel i e percorre o Array inteiro, e depois ele incrementa mais um na variavel original index = 0;
+        if (index == Amigos.length - 1){ //Se o index (valor selecionado) for igual ao comprimento do Array, tire o nome da lista
+        sorteio.innerHTML = sorteio.innerHTML + Amigos[index] + '--->' + Amigos[0] + '<br>' //Adiciona os nomes já sorteados com os nomes que serao sorteados logo depois                  Array Amigo[Nome sorteado]
         }else {
             sorteio.innerHTML = sorteio.innerHTML + Amigos[index] + '--->' + Amigos[index + 1] + '<br>'   //Pega a Lista de Nomes sorteados, e modifica, colocando o primeiro nome sorteado, depois adicionando o proximo nome sorteado Amigos[index + 1] (Amigos é o Array)
         }
